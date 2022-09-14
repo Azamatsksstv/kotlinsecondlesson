ccae37a61054366854fdcb98a744d7d48afbd02d
Azamatskkstv <azambylbaev@gmail.com>
2022-09-14 15:49:04 +0500

    3.2.3

--- src/main/kotlin/Hello.kt
+++ src/main/kotlin/Hello.kt
@@ -1,15 +1,4 @@
-import java.util.*
-
-fun feedTheFish() {
-    val day = randomDay()
-    val food = fishFood(day)
-
-    println ("Today is $day and the fish eat $food")
-}
-
-fun main(args: Array<String>) {
-    feedTheFish()
-}
+import java.util.*    // required import
 
 fun randomDay() : String {
     val week = arrayOf ("Monday", "Tuesday", "Wednesday", "Thursday",
@@ -18,16 +7,22 @@ fun randomDay() : String {
 }
 
 fun fishFood (day : String) : String {
-    var food = ""
-    when (day) {
-        "Monday" -> food = "flakes"
-        "Tuesday" -> food = "pellets"
-        "Wednesday" -> food = "redworms"
-        "Thursday" -> food = "granules"
-        "Friday" -> food = "mosquitoes"
-        "Saturday" -> food = "lettuce"
-        "Sunday" -> food = "plankton"
+    return when (day) {
+        "Monday" -> "flakes"
+        "Wednesday" -> "redworms"
+        "Thursday" -> "granules"
+        "Friday" -> "mosquitoes"
+        "Sunday" -> "plankton"
+        else -> "nothing"
     }
-    return food
 }
 
+fun feedTheFish() {
+    val day = randomDay()
+    val food = fishFood(day)
+    println ("Today is $day and the fish eat $food")
+}
+
+fun main(args: Array<String>) {
+    feedTheFish()
+}
