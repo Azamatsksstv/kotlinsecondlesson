d0895015bc9da10a75666c8a457288a34f89e626
Azamatskkstv <azambylbaev@gmail.com>
2022-09-14 15:57:39 +0500

    4.2.2

--- src/main/kotlin/Hello.kt
+++ src/main/kotlin/Hello.kt
@@ -21,13 +21,22 @@ fun feedTheFish() {
     val day = randomDay()
     val food = fishFood(day)
     println ("Today is $day and the fish eat $food")
+    println("Change water: ${shouldChangeWater(day)}")
 }
 
 fun main(args: Array<String>) {
-    swim()   // uses default speed
-    swim("slow")   // positional argument
-    swim(speed="turtle-like")
+    feedTheFish()
 }
 fun swim(speed: String = "fast") {
     println("swimming $speed")
 }
+
+fun shouldChangeWater (day: String, temperature: Int = 22, dirty: Int = 20): Boolean {
+    return when {
+        temperature > 30 -> true
+        dirty > 30 -> true
+        day == "Sunday" ->  true
+        else -> false
+    }
+}
+
